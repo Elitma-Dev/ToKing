3e6781a4ea071f986667a75accd504194276e8ef
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2020-09-13 11:15:00 -0300

    Adding Koin to use Dependency Injection (#11)
    * Adding koin
    
    * Starting Koin with first module
    
    * Using Custom Application as Application default
    
    * Reformating code
    
    * Adding unused

---
+++ app/src/main/java/br/com/prado/toking/CustomApplication.kt
@@ -0,0 +1,16 @@
+package br.com.prado.toking
+
+import android.app.Application
+import br.com.prado.toking.di.appModule
+import org.koin.core.context.startKoin
+
+@Suppress("unused")
+class CustomApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            appModule
+        }
+    }
+}
