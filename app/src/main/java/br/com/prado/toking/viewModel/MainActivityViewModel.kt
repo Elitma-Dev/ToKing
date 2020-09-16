2c4bb147d577fbb9f14485ad4266996ab0d17d93
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2020-09-16 20:18:38 -0300

    Adding actions

--- app/src/main/java/br/com/prado/toking/viewModel/MainActivityViewModel.kt
+++ app/src/main/java/br/com/prado/toking/viewModel/MainActivityViewModel.kt
@@ -1,5 +1,22 @@
 package br.com.prado.toking.viewModel
 
+import SingleLiveEvent
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel()
+internal class MainActivityViewModel : ViewModel() {
+
+    internal sealed class Action {
+        internal data class SelectPageByNavigation(val position: Int) : Action()
+        internal data class SelectPageByViewPager(val position: Int) : Action()
+    }
+
+    internal val action = SingleLiveEvent<Action>()
+
+    fun onPageSelected(position: Int) {
+        action.value = Action.SelectPageByNavigation(position)
+    }
+
+    fun onNavigationItemSelected(position: Int) {
+        action.value = Action.SelectPageByViewPager(position)
+    }
+}
