d7e414cac30c5db8f200d0947526a4002c94113d
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2020-09-16 20:19:09 -0300

    Adding modules to Koin

--- app/src/main/java/br/com/prado/toking/CustomApplication.kt
+++ app/src/main/java/br/com/prado/toking/CustomApplication.kt
@@ -10,7 +10,7 @@ class CustomApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            appModule
+            modules(appModule)
         }
     }
 }
