018c3a7b45cac25aacaaeaaee3c6428cb646d3b9
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2020-09-16 20:19:02 -0300

    Providing MainActivityViewModel with Koin ViewModel

--- app/src/main/java/br/com/prado/toking/di/AppModule.kt
+++ app/src/main/java/br/com/prado/toking/di/AppModule.kt
@@ -1,7 +1,9 @@
 package br.com.prado.toking.di
 
+import br.com.prado.toking.viewModel.MainActivityViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-
+    viewModel { MainActivityViewModel() }
 }
